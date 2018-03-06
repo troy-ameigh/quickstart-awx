e4a4907bba292273423c163750da7a3b97955f5e
Andrew Gargan <gargana@amazon.com>
2018-03-06 22:53:19 +0200

    cleaned out more unneeded files

--- scripts/cmd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-aws cloudformation create-stack \
-  --stack-name "AWXTest" \
-  --template-body file://./templates/awx-master.template \
-  --parameters file://./ci/dev-gargana.json \
-  --capabilities "CAPABILITY_IAM" \
-  --disable-rollback  
-
